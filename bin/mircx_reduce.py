01254eb49e1797f5c75484a4656de1ce5abbac54
Jean-Baptiste Le Bouquin <jean-baptiste.lebouquin@univ-grenoble-alpes.fr>
2018-03-23 17:31:57 -0400

    Remove useless option in mircx_reduce.py

--- bin/mircx_reduce.py
+++ bin/mircx_reduce.py
@@ -16,12 +16,27 @@ from mircx_pipeline import log, setup;
 description = \
 """
 description:
-  Run the mircx test pipeline. Shall be ran in the directory
+  Run the mircx pipeline. Shall be ran in the directory
   of the RAW data. The data format shall be .fits and/or .fits.fz
   The format .fits.gz is not supported.
 
-  By default, outputs are written in directories preproc/ vis/
-  and viscalib/
+  By default, outputs are written in the following directories:
+
+  preproc/ contains the BACKGROUND, BEAM, PREPROC and SPEC_CAL
+           intermediate products, which mostly corresponds to
+           detector images cleaned from the detector artifact
+           (step 1 of reduction).
+
+  rts/     contains RTS intermediate products, which are the
+           coherent flux and the photometric flux in real time,
+           cleaned from the instrumental behavior, but not yet
+           averaged (step 2 of reduction).
+
+  vis/     contains the final OIFITS products, which are the 
+           uncalibrated mean visibilities and closure phases
+           computed by selecting and averaging the data in RTS
+           (step 3 of reduction).
+
 """
 
 epilog = \
@@ -32,7 +47,8 @@ examples:
 """
 
 parser = argparse.ArgumentParser (description=description, epilog=epilog,
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter);
+                                 formatter_class=argparse.RawDescriptionHelpFormatter);
+
 
 TrueFalse = ['TRUE','FALSE'];
 TrueFalseOverwrite = ['TRUE','FALSE','OVERWRITE'];
@@ -43,65 +59,44 @@ parser.add_argument ("--debug", dest="debug",default='FALSE',
 
 parser.add_argument ("--max-file", dest="max_file",default=3000,type=int,
                      help="maximum number of file to load to build "
-                          "product (speed-up for tests)");
+                          "product (speed-up for tests) [%(default)s]");
 
 
 parser.add_argument ("--raw-dir", dest="raw_dir",default='./',type=str,
-                     help="directory of raw data");
+                     help="directory of raw data [%(default)s]");
 
 parser.add_argument ("--preproc-dir", dest="preproc_dir",default='./preproc/',type=str,
-                     help="directory of intermediate products");
+                     help="directory of products [%(default)s]");
 
 parser.add_argument ("--rts-dir", dest="rts_dir",default='./rts/',type=str,
-                     help="directory of intermediate products");
+                     help="directory of products [%(default)s]");
 
 parser.add_argument ("--vis-dir", dest="vis_dir",default='./vis/',type=str,
-                     help="directory of intermediate products");
+                     help="directory of products [%(default)s]");
 
-parser.add_argument ("--vis-calibrated-dir", dest="viscalib_dir",default='./viscalib/',type=str,
-                     help="directory of intermediate products");
-
-
-parser.add_argument ("--background", dest="background",default='TRUE',
-                     choices=TrueFalseOverwrite,
-                     help="compute the BACKGROUND products");
-
-parser.add_argument ("--beam-map", dest="bmap",default='TRUE',
-                     choices=TrueFalseOverwrite,
-                     help="compute the BEAM_MAP products");
 
 parser.add_argument ("--preproc", dest="preproc",default='TRUE',
                      choices=TrueFalseOverwrite,
-                     help="compute the PREPROC products");
-
-parser.add_argument ("--spec-cal", dest="speccal",default='TRUE',
-                     choices=TrueFalseOverwrite,
-                     help="compute the SPEC_CAL products");
+                     help="compute the PREPROC products [%(default)s]");
 
 parser.add_argument ("--rts", dest="rts",default='TRUE',
                      choices=TrueFalseOverwrite,
-                     help="compute the RTS products");
+                     help="compute the RTS products [%(default)s]");
 
 parser.add_argument ("--vis", dest="vis",default='TRUE',
                      choices=TrueFalseOverwrite,
-                     help="compute the VIS products");
-
-parser.add_argument ("--vis-calibrated", dest="viscalib",default='FALSE',
-                     choices=TrueFalseOverwrite,
-                     help="compute the VIS_CALIBRATED products");
+                     help="compute the VIS products [%(default)s]");
 
 
 parser.add_argument ("--beam-quality", dest="beam_quality", type=float,
-                    default=2.0, help="minimum quality to consider the beammap as valid");
+                    default=2.0, help="minimum quality to consider the beammap as valid [%(default)s]");
 
 parser.add_argument ("--ncoherent", dest="ncoherent", type=float,
-                    default=2.0, help="number of frames (can be fractional) for coherent integration");
+                    default=2.0, help="number of frames for coherent integration, can be fractional [%(default)s]");
 
 parser.add_argument ("--snr-threshold", dest="snr_threshold", type=float,
-                    default=2.0, help="SNR threshold for fringe selection");
+                    default=2.0, help="SNR threshold for fringe selection [%(default)s]");
 
-parser.add_argument ("--calibrators", dest="calibrators",default='name1,diam,err,name2,diam,err',
-                     type=str, help="list of calibration star with diameters");
 
 #
 # Initialisation
@@ -113,16 +108,16 @@ argopt = parser.parse_args ();
 # Verbose
 elog = log.trace ('mircx_reduce');
 
-#
-# Compute BACKGROUND_MEAN
-#
-
-if argopt.background != 'FALSE':
-    overwrite = (argopt.background == 'OVERWRITE');
-
-    # List inputs
-    hdrs = hdrs_raw = mrx.headers.loaddir (argopt.raw_dir);
+# List inputs
+hdrs = hdrs_raw = mrx.headers.loaddir (argopt.raw_dir);
     
+if argopt.preproc != 'FALSE':
+    overwrite = (argopt.preproc == 'OVERWRITE');
+
+    #
+    # Compute BACKGROUND_MEAN
+    #
+
     # Group backgrounds
     keys = setup.detwin + setup.detmode + setup.insmode;
     gps = mrx.headers.group (hdrs, 'BACKGROUND', keys=keys,
@@ -149,15 +144,11 @@ if argopt.background != 'FALSE':
         finally:
             log.closeFile ();
 
-#
-# Compute BEAMi_MAP
-#
+    #
+    # Compute BEAMi_MAP
+    #
 
-if argopt.bmap != 'FALSE':
-    overwrite = (argopt.bmap == 'OVERWRITE');
-        
     # List inputs
-    hdrs = mrx.headers.loaddir (argopt.raw_dir);
     hdrs_calib = mrx.headers.loaddir (argopt.preproc_dir);
     
     # Group all BEAMi
@@ -192,15 +183,11 @@ if argopt.bmap != 'FALSE':
         finally:
             log.closeFile ();
         
-#
-# Compute PREPROC
-#
-
-if argopt.preproc != 'FALSE':
-    overwrite = (argopt.preproc == 'OVERWRITE');
+    #
+    # Compute PREPROC
+    #
 
     # List inputs
-    hdrs = mrx.headers.loaddir (argopt.raw_dir);
     hdrs_calib = mrx.headers.loaddir (argopt.preproc_dir);
 
     # Group all DATA
@@ -242,12 +229,9 @@ if argopt.preproc != 'FALSE':
         finally:
             log.closeFile ();
 
-#
-# Compute SPEC_CAL
-#
-
-if argopt.speccal != 'FALSE':
-    overwrite = (argopt.speccal == 'OVERWRITE');
+    #
+    # Compute SPEC_CAL
+    #
 
     # List inputs
     hdrs = mrx.headers.loaddir (argopt.preproc_dir);
@@ -369,40 +353,3 @@ if argopt.vis != 'FALSE':
         finally:
             log.closeFile ();
             
-                                    
-#
-# Compute VIS_CALIBRATED
-#
-
-if argopt.viscalib != 'FALSE':
-    overwrite = (argopt.viscalib == 'OVERWRITE');
-
-    # Read all calibration products, keep only the VIS
-    hdrs = mrx.headers.loaddir (argopt.vis_dir);
-
-    # Group all VIS by calibratable setup
-    keys = setup.detwin + setup.insmode + setup.fringewin + setup.visparam;
-    gps = mrx.headers.group (hdrs, 'VIS', delta=1e9, Delta=1e9,
-                             keys=keys, continuous=False);
-
-    # Parse input catalog
-    catalog = mrx.headers.parse_argopt_catalog (argopt.calibrators);
-
-    # Compute 
-    for i,gp in enumerate (gps):
-        try:
-            log.info ('Calibrate setup {0} over {1} '.format(i+1,len(gps)));
-
-            # Create output directory and set log
-            mrx.files.ensure_dir (argopt.viscalib_dir);
-            log.setFile (argopt.viscalib_dir+'/calibration_setup%i.log'%i);
-            
-            outputSetup = 'calibration_setup%i'%i;
-            mrx.compute_all_viscalib (gp, catalog, outputDir=argopt.viscalib_dir,
-                                      outputSetup=outputSetup);
-
-        except Exception as exc:
-            log.error ('Cannot calibrate setup: '+str(exc));
-            if argopt.debug == 'TRUE': raise;
-        finally:
-            log.closeFile ();
