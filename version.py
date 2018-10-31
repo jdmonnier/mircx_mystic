91fb58692327b4e3c89205c85c5ddba538256139
Jean-Baptiste Le Bouquin <jean-baptiste.lebouquin@univ-grenoble-alpes.fr>
2018-10-30 20:07:05 -0400

    Version 0.9.9

--- version.py
+++ version.py
@@ -5,5 +5,5 @@ def info ():
     print ('Please report any bug to lebouquj@umich.edu');
     print ('---------------------------------------------');
     
-revision = '0.9.8';
+revision = '0.9.9';
 
