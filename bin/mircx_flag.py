1beadf61289e5b3e53a4da8e81f367b36d598edb
Jean-Baptiste Le Bouquin <jean-baptiste.lebouquin@univ-grenoble-alpes.fr>
2019-02-20 09:09:32 -0500

    Improve the flagging system

--- bin/mircx_flag.py
+++ bin/mircx_flag.py
@@ -18,14 +18,28 @@ description = \
 """
 description:
 
+Use this script to flag (=reject) data based on time,
+baseline, target name, and wavelength. The script create
+new files in the output-dir.
+
+By default, the script parse all *.fits files in the current
+directory, but a list of input file(s) can be specified instead.
+
+TODO: I want to evolve the script to handle
+multiple time interval, to better handle the baseline
+(swap order)...
+
 """
 
 epilog = \
 """
 examples:
 
+  mircx_flag.py --base S1S2 --target HD1234 --output-dir=all_flagged_data/
 
+  mircx_flag.py --base W1 --target HD1234 mircx00124_oifits.fits
 
+contact lebouquj@umich.edu for support.
 """
 
 parser = argparse.ArgumentParser (description=description, epilog=epilog,
@@ -36,31 +50,39 @@ parser = argparse.ArgumentParser (description=description, epilog=epilog,
 TrueFalse = ['TRUE','FALSE'];
 TrueFalseOverwrite = ['TRUE','FALSE','OVERWRITE'];
 
-parser.add_argument ("--lbd", dest="lbd", default=[1.,3.],
+# Opional arguments
+parser.add_argument ('--lbd', dest='lbd', default=[1.,3.],
                      type=float, nargs=2,
-                     help="interval of wavelength in um (default is all)");
+                     help='interval of wavelength in um (default is all)');
 
-parser.add_argument ("--mjd", dest="mjd", default=[50000,60000],
+parser.add_argument ('--mjd', dest='mjd', default=[50000,60000],
                      type=float, nargs=2,
-                     help="time interval in modified julian day (default is all)");
+                     help='time interval in modified julian day (default is all)');
 
-parser.add_argument ("--target", dest="target", default=['INTERNAL'],
-                     type=str, nargs="+",
-                     help="list of target (default is none)");
+parser.add_argument ('--target', dest='target', default=['INTERNAL'],
+                     type=str, nargs='+',
+                     help='list of target (default is none)');
 
-parser.add_argument ("--base", dest="base",
-                     type=str, nargs="+",
-                     help="list of baseline (e.g S1S2, default is none)");
+parser.add_argument ('--base', dest='base',
+                     type=str, nargs='+',
+                     help='list of baseline (e.g S1S2, default is none)');
 
-parser.add_argument ("--output-dir", dest="output_dir", default='./flagged/',
+parser.add_argument ('--output-dir', dest='output_dir', default='./flagged/',
                      type=str,
-                     help=" def: %(default)s");
+                     help=' def: %(default)s');
+
+parser.add_argument ("--debug", dest="debug",default='FALSE',
+                     choices=TrueFalse,
+                     help="stop on error [%(default)s]");
 
 parser.add_argument ('--help', action='help',
                      help=argparse.SUPPRESS);
 
 parser.add_argument ('-h', action='help',
                      help=argparse.SUPPRESS);
+
+# Positional argument
+parser.add_argument ('input_files', type=str, nargs='*', default=['*_oifits.fits']);
 
 # Parse argument
 argopt = parser.parse_args ();
@@ -75,7 +97,7 @@ if len (argopt.lbd) % 2:
 if len (argopt.mjd) % 2:
     raise ValueError ('mjd should have even number of values');
 
-# Function to explore OIFITS
+# Function to associate tables in OIFITS
 def get_wave (hdulist, hdu):
     n = hdu.header['INSNAME'];
     for h in hdulist:
@@ -93,46 +115,54 @@ def get_target (hdulist):
         if h.header.get ('EXTNAME') == 'OI_TARGET':
             return dict([(d['TARGET_ID'],d['TARGET']) for d in h.data]);
 
+# Define input list of files
+inputs = [];
+for l in argopt.input_files: inputs += glob.glob(l);
+
 # Loop on list of files
-inputs = glob.glob ('*.fits');
 for file in inputs:
 
     # Open file
     log.info ('File ' + file);
-    hdulist = pyfits.open (file);
-
-    # Get the target dictionnary for this file
-    trgdic = get_target (hdulist);
-
-    # Loop on extensions
-    for hdu in hdulist:
-        if hdu.header.get('EXTNAME') not in ['OI_VIS2', 'OI_VIS', 'OI_T3']: continue;
-
-        # Get the station dictionary and the lbd for this table
-        stadic = get_station (hdulist, hdu);
-        lbd = get_wave (hdulist, hdu) * 1e6;
+    try:
+        hdulist = pyfits.open (file);
         
-        # Loop on data line by line
-        for data in hdu.data:
-
-            # Check time
-            mjd = data['MJD'];
-            if mjd < argopt.mjd[0] or mjd > argopt.mjd[1]: continue;
-            
-            # Check target
-            trg = trgdic[data['TARGET_ID']];
-            if argopt.target is not None and trg not in argopt.target: continue;
-
-            # Check baseline
-            base = ''.join([stadic[i] for i in data['STA_INDEX']]);
-            if argopt.base is not None and base not in argopt.base: continue;
-            
-            # Check wavelength
-            ids = (lbd >= argopt.lbd[0]) * (lbd <= argopt.lbd[1]);
-            data['FLAG'] += ids;
-
-            log.info ('Flag %-7s %.4f %s %s'%(hdu.header['EXTNAME'], mjd, trg, base));
+        # Get the target dictionnary for this file
+        trgdic = get_target (hdulist);
         
-    # Save
-    files.write (hdulist, argopt.output_dir+file);
-    hdulist.close ();
+        # Loop on extensions
+        for hdu in hdulist:
+            if hdu.header.get('EXTNAME') not in ['OI_VIS2', 'OI_VIS', 'OI_T3']: continue;
+        
+            # Get the station dictionary and the lbd for this table
+            stadic = get_station (hdulist, hdu);
+            lbd = get_wave (hdulist, hdu) * 1e6;
+            
+            # Loop on data line by line
+            for data in hdu.data:
+        
+                # Check time
+                mjd = data['MJD'];
+                if mjd < argopt.mjd[0] or mjd > argopt.mjd[1]: continue;
+                
+                # Check target
+                trg = trgdic[data['TARGET_ID']];
+                if argopt.target is not None and trg not in argopt.target: continue;
+        
+                # Check baseline
+                base = ''.join([stadic[i] for i in data['STA_INDEX']]);
+                if argopt.base is not None and base not in argopt.base: continue;
+                
+                # Check wavelength
+                ids = (lbd >= argopt.lbd[0]) * (lbd <= argopt.lbd[1]);
+                data['FLAG'] += ids;
+        
+                log.info ('Flag %-7s %.4f %s %s'%(hdu.header['EXTNAME'], mjd, trg, base));
+            
+        # Save
+        files.write (hdulist, argopt.output_dir+file);
+        hdulist.close ();
+        
+    except Exception as exc:
+        log.error ('Cannot deal with file '+str(file));
+        if argopt.debug == 'TRUE': raise;
