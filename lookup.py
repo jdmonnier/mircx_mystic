a5c54c26dabcbc8dfdaa1238d0dea37986a3acb0
Claire Davies <c.davies3@exeter.ac.uk>
2019-05-08 17:20:38 +0100

    account for whether final character in $MIRCX_PIPELINE is "/" or not

--- lookup.py
+++ lookup.py
@@ -205,7 +205,10 @@ def queryLocal(targs,db):
                 outline = targ.replace('_',' ')+','+calsci+'\n'
                 scical.append('NEW:CAL')
                 calInf = calInf+targ.replace(' ','_')+','+','.join(calsci.split(',')[6:8])+','
-            outfile = os.environ['MIRCX_PIPELINE']+'mircx_pipeline/mircx_newTargs.list'
+            if os.environ['MIRCX_PIPELINE'][-1] != '/':
+                outfile = os.environ['MIRCX_PIPELINE']+'/mircx_pipeline/mircx_newTargs.list'
+            else:
+                outfile = os.environ['MIRCX_PIPELINE']+'mircx_pipeline/mircx_newTargs.list'
             if not os.path.exists(outfile):
                 with open(outfile, 'w') as output:
                     output.write('#NAME,RA,DEC,HMAG,VMAG,ISCAL,MODEL_NAME,PARAM1,PARAM2\n')
