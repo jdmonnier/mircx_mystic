453e65023abfd8815af82960d8625118d1a0c9e4
Claire Davies <c.davies3@exeter.ac.uk>
2019-07-09 09:00:36 +0100

    updated import commands for anaconda3 compatibility

--- mailfile.py
+++ mailfile.py
@@ -1,8 +1,8 @@
 import sys, os
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEBase import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email..mime.base import MIMEBase
 from email import encoders
 
 from . import log
