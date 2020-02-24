12d3d4e0e16feb2c0f702a023be08e59c9770ae0
Narsireddy Anugu <nanugu@chara-array.org>
2020-02-24 13:04:47 +0000

    ensured fitting results as well as outcome returned from function

--- inspect.py
+++ inspect.py
@@ -1,3 +1,4 @@
+from . import log
 
 def calTest(files, UDD, obj, outDir, uset3amp=False, fixUDD=True, detLim=True):
     """
@@ -39,7 +40,7 @@ def calTest(files, UDD, obj, outDir, uset3amp=False, fixUDD=True, detLim=True):
             print('    python setup.py install --user')
             print('in your CANDID directory.')
             print(' ')
-        return 'failed'
+        return ['Import Error', 0]
     
     try:
         import cyvis
@@ -47,46 +48,61 @@ def calTest(files, UDD, obj, outDir, uset3amp=False, fixUDD=True, detLim=True):
         print('CANDID not installed. Use:')
         print(' python setup.py install --user')
         print(' in CANDID directory')
-        return 'failed'
+        return ['Import Error', 0]
     
     import matplotlib.pyplot as plt
     
     o = candid.Open(files)
+    log.info('Read files for '+str(obj)+' into CANDID')
     if uset3amp == False:
         o.observables = ['v2', 'cp']
     
     if fixUDD == True:
+        log.info('Running CANDID fitMap with fixed UDD')
         o.fitMap(fig=0, addParam={'diam*':float(UDD)}, doNotFit=['diam*'])
         plt.figure(0)
         plt.savefig(outDir+'/'+obj+'_fitMap_fixUDD.pdf')
+        log.info('Write '+outDir+'/'+obj+'_fitMap_fixUDD.pdf')
         plt.close()
         plt.figure(1)
-        plt.savefig(outDir+'/'+obj+'_Residuals_fitUDD.pdf')
+        plt.savefig(outDir+'/'+obj+'_Residuals_fixUDD.pdf')
+        log.info('Write '+outDir+'/'+obj+'_Residuals_fixUDD.pdf')
         plt.close()
         ret = 'fixed'
     else:
-        o.fitMap(fig=0)
+        try:
+            log.info('Running CANDID fitMap with UDD as free parameter')
+            o.fitMap(fig=0)
+        except TypeError as exception:
+            log.error('Error encountered in CANDID: '+str(exception))
+            return ['failed', 0]
         plt.figure(0)
         plt.savefig(outDir+'/'+obj+'_fitMap_fitUDD.pdf')
+        log.info('Write '+outDir+'/'+obj+'_fitMap_fitUDD.pdf')
         plt.close()
         plt.figure(1)
         plt.savefig(outDir+'/'+obj+'_Residuals_fitUDD.pdf')
+        log.info('Write '+outDir+'/'+obj+'_Residuals_fitUDD.pdf')
         plt.close()
         # comment on how this differs from mircx_target.list value
         fitUDD, eUDD = o.bestFit['best']['diam*'], o.bestFit['uncer']['diam*']
-        if UDD < (fitUDD + eUDD) and UDD > (fitUDD - eUDD):
+        if float(UDD) < (fitUDD + eUDD) and float(UDD) > (fitUDD - eUDD):
             ret = 'within range'
         else:
             ret = 'warning'
     
     if detLim == True:
         p = o.bestFit['best']
-        candid.CONFIG['long exec warning'] = 900
+        candid.CONFIG['long exec warning'] = 2000
+        log.info('Running CANDID detectionLimit with companion removed')
         o.detectionLimit(fig=2, removeCompanion=p, methods=['injection'])
         plt.figure(2)
         plt.plot([o.rmin, o.rmax], [-2.5*np.log10(p['f']/100.)]*2, ls='--', color='k')
         plt.plot([np.sqrt(p['x']**2+p['y']**2)], [-2.5*np.log10(p['f']/100.)], ls=None, marker='*', ms=8)
+        #if -2.5*np.log10(p['f']/100.) < yl[0]:
+        #    ax.set_ylim((-2.5*np.log10(p['f']/100.))-0.2, yl[1]) # ensure companion can be seen on plot
         plt.savefig(outDir+'/'+obj+'_detLim.pdf')
+        log.info('Write '+outDir+'/'+obj+'_detLim.pdf')
         plt.close()
     
-    return ret
+    return [ret, p]
